fc4ab8bb20a0d1b0c5678e9139ca1285fa448167
Omer <omerinci415@gmail.com>
2023-03-27 23:10:31 +0300

    fdsfsd

--- part1/book/api/urls.py
+++ part1/book/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import book_list_api_view
+from .views import book_list_api_view,book_detail_api_view
 
 urlpatterns = [
     path('book/', book_list_api_view, name='book-list'),
+    path('book/<int:pk>/', book_detail_api_view, name='book-detail'),
 ]
--- part1/book/api/views.py
+++ part1/book/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -6,11 +7,49 @@ from ..models import Book
 from .serializers import BookSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def book_list_api_view(request):
-    book = Book.objects.all()
-    serializer = BookSerializer(book, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        book = Book.objects.all()
+        serializer = BookSerializer(book, many=True)  # many=True birder fazla queryset gönderince yazılır
+        return Response(serializer.data)
 
 
 
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def book_detail_api_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+
+
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return Response(
+            {
+                'işlem': {
+                    'code': 204,
+                    'message': f'({pk}) id numaralı makale silinmiştir.'
+                }
+            },
+            status=status.HTTP_204_NO_CONTENT
+        )
