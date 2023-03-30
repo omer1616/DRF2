64b108251461bc047fd7d164882b63a584794194
Omer <omerinci415@gmail.com>
2023-03-30 19:04:07 +0300

    added product

---
+++ part2/product/api/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from ..models import Product, Category
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+
+
+class CategorySerializers(serializers.ModelSerializer):
+
+    products =  ProductSerializer(many=True,read_only=True )
+    class Meta:
+        model = Category
+        fields = "__all__"
--- part2/product/models.py
+++ part2/product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    title = models.TextField(max_length=150)
+    price = models.IntegerField()
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name="products")
+    created_date = models.DateField(auto_now_add=True)
+    is_stock = models.BooleanField(verbose_name="Stok", default=True)
